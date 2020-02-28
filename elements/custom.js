027e4290192f0d242f64f1aca2d40e8ea7fae3d4
Jérémy Lal <kapouer@melix.org>
2020-02-28 16:09:32 +0100

    Initial files

---
+++ elements/custom.js
@@ -0,0 +1,13 @@
+exports.custom = {
+	priority: 100,
+	group: 'block',
+	stylesheets: [
+		'https://fonts.googleapis.com/css?family=Pacifico&display=swap',
+		'../ui/custom.css'
+	],
+	csp: {
+		font: ["https://fonts.gstatic.com"]
+	}
+};
+
+
